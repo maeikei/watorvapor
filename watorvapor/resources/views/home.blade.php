6305fe8a7c86a0e3ba9dc744d75564f6001f8908
ma yingkui <maeikei@gmail.com>
2015-06-03 22:57:02 +0900

    Update home.blade.php

--- watorvapor/resources/views/home.blade.php
+++ watorvapor/resources/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends('app')
 
 @section('content')
-<div class="container">
+<!--div class="container">
 	<div class="row">
 		<div class="col-md-10 col-md-offset-1">
 			<div class="panel panel-default">
@@ -13,5 +13,5 @@
 			</div>
 		</div>
 	</div>
-</div>
+</div-->
 @endsection
