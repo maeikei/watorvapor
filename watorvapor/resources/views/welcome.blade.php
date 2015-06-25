045dee8749c560de6b2da9305373da12abd9865d
ma yingkui <maeikei@gmail.com>
2015-06-25 23:01:58 +0900

    change height

--- watorvapor/resources/views/welcome.blade.php
+++ watorvapor/resources/views/welcome.blade.php
@@ -4,57 +4,57 @@
 <div class="container">
 	<div class="row">
 		<div class="col-md-10 col-md-offset-1">
-			<a href="{{ asset('assets/img/brain.orig.png') }}" ><img height="60" src="{{ asset('assets/img/brain.orig.png') }}"><a>
-			<a href="{{ asset('assets/img/brain.header.jpg') }}" ><img height="60" src="{{ asset('assets/img/brain.header.jpg') }}"><a>
-			<a href="{{ asset('assets/img/brain.king.jpg') }}" ><img height="60" src="{{ asset('assets/img/brain.king.jpg') }}"><a>
-			<a href="{{ asset('assets/img/brain.skelton.jpg') }}" ><img height="60" src="{{ asset('assets/img/brain.skelton.jpg') }}"><a>
-			<a href="{{ asset('assets/img/digital.brain.reading.jpg') }}" ><img height="60" src="{{ asset('assets/img/digital.brain.reading.jpg') }}"><a>
+			<a href="{{ asset('assets/img/brain.orig.png') }}" ><img height="80" src="{{ asset('assets/img/brain.orig.png') }}"><a>
+			<a href="{{ asset('assets/img/brain.header.jpg') }}" ><img height="80" src="{{ asset('assets/img/brain.header.jpg') }}"><a>
+			<a href="{{ asset('assets/img/brain.king.jpg') }}" ><img height="80" src="{{ asset('assets/img/brain.king.jpg') }}"><a>
+			<a href="{{ asset('assets/img/brain.skelton.jpg') }}" ><img height="80" src="{{ asset('assets/img/brain.skelton.jpg') }}"><a>
+			<a href="{{ asset('assets/img/digital.brain.reading.jpg') }}" ><img height="80" src="{{ asset('assets/img/digital.brain.reading.jpg') }}"><a>
 		</div>
 	</div>
 	<div class="row">
 		<div class="col-md-10 col-md-offset-1">
-			<a href="{{ asset('assets/img/ai.nl.jpg') }}" ><img height="60" src="{{ asset('assets/img/ai.nl.jpg') }}"><a>
-			<a href="{{ asset('assets/img/ai.dnn.jpg') }}" ><img height="60" src="{{ asset('assets/img/ai.dnn.jpg') }}"><a>
-			<a href="{{ asset('assets/img/ai.dream.jpg') }}" ><img height="60" src="{{ asset('assets/img/ai.dream.jpg') }}"><a>
-			<a href="{{ asset('assets/img/i.am.robot.png') }}" ><img height="60" src="{{ asset('assets/img/i.am.robot.png') }}"><a>
+			<a href="{{ asset('assets/img/ai.nl.jpg') }}" ><img height="80" src="{{ asset('assets/img/ai.nl.jpg') }}"><a>
+			<a href="{{ asset('assets/img/ai.dnn.jpg') }}" ><img height="80" src="{{ asset('assets/img/ai.dnn.jpg') }}"><a>
+			<a href="{{ asset('assets/img/ai.dream.jpg') }}" ><img height="80" src="{{ asset('assets/img/ai.dream.jpg') }}"><a>
+			<a href="{{ asset('assets/img/i.am.robot.png') }}" ><img height="80" src="{{ asset('assets/img/i.am.robot.png') }}"><a>
 		</div>
 	</div>
 	<div class="row">
 		<div class="col-md-10 col-md-offset-1">
-			<a href="{{ asset('assets/img/digital.Consciousness.reading.jpg') }}" ><img height="60" src="{{ asset('assets/img/digital.Consciousness.reading.jpg') }}"><a>
-			<a href="{{ asset('assets/img/digital.brain.block.jpg') }}" ><img height="60" src="{{ asset('assets/img/digital.brain.block.jpg') }}"><a>
-			<a href="{{ asset('assets/img/digital.soul.jpg') }}" ><img height="60" src="{{ asset('assets/img/digital.soul.jpg') }}"><a>
-			<a href="{{ asset('assets/img/digital.Consciousness.binary.jpg') }}" ><img height="60" src="{{ asset('assets/img/digital.Consciousness.binary.jpg') }}"><a>
-			<a href="{{ asset('assets/img/digital.brain.block.3d.jpg') }}" ><img height="60" src="{{ asset('assets/img/digital.brain.block.3d.jpg') }}"><a>
+			<a href="{{ asset('assets/img/digital.Consciousness.reading.jpg') }}" ><img height="80" src="{{ asset('assets/img/digital.Consciousness.reading.jpg') }}"><a>
+			<a href="{{ asset('assets/img/digital.brain.block.jpg') }}" ><img height="80" src="{{ asset('assets/img/digital.brain.block.jpg') }}"><a>
+			<a href="{{ asset('assets/img/digital.soul.jpg') }}" ><img height="80" src="{{ asset('assets/img/digital.soul.jpg') }}"><a>
+			<a href="{{ asset('assets/img/digital.Consciousness.binary.jpg') }}" ><img height="80" src="{{ asset('assets/img/digital.Consciousness.binary.jpg') }}"><a>
+			<a href="{{ asset('assets/img/digital.brain.block.3d.jpg') }}" ><img height="80" src="{{ asset('assets/img/digital.brain.block.3d.jpg') }}"><a>
 		</div>
 	</div>
 	<div class="row">
 		<div class="col-md-10 col-md-offset-1">
-			<a href="{{ asset('assets/img/robot.brain.vista.jpg') }}" ><img height="60" src="{{ asset('assets/img/robot.brain.vista.jpg') }}"><a>
-			<a href="{{ asset('assets/img/robot.brain.why.jpg') }}" ><img height="60" src="{{ asset('assets/img/robot.brain.why.jpg') }}"><a>
-			<a href="{{ asset('assets/img/robot.brain.white.jpg') }}" ><img height="60" src="{{ asset('assets/img/robot.brain.white.jpg') }}"><a>
-			<a href="{{ asset('assets/img/robot.thinking.pose.jpg') }}" ><img height="60" src="{{ asset('assets/img/robot.thinking.pose.jpg') }}"><a>
+			<a href="{{ asset('assets/img/robot.brain.vista.jpg') }}" ><img height="80" src="{{ asset('assets/img/robot.brain.vista.jpg') }}"><a>
+			<a href="{{ asset('assets/img/robot.brain.why.jpg') }}" ><img height="80" src="{{ asset('assets/img/robot.brain.why.jpg') }}"><a>
+			<a href="{{ asset('assets/img/robot.brain.white.jpg') }}" ><img height="80" src="{{ asset('assets/img/robot.brain.white.jpg') }}"><a>
+			<a href="{{ asset('assets/img/robot.thinking.pose.jpg') }}" ><img height="80" src="{{ asset('assets/img/robot.thinking.pose.jpg') }}"><a>
 		</div>
 	</div>
 	<div class="row">
 		<div class="col-md-10 col-md-offset-1">
-			<a href="{{ asset('assets/img/robot.rocket.block.jpg') }}" ><img height="60" src="{{ asset('assets/img/robot.rocket.block.jpg') }}"><a>
-			<a href="{{ asset('assets/img/robot.star.space.jpg') }}" ><img height="60" src="{{ asset('assets/img/robot.star.space.jpg') }}"><a>
-			<a href="{{ asset('assets/img/space.travel.jpg') }}" ><img height="60" src="{{ asset('assets/img/space.travel.jpg') }}"><a>
-			<a href="{{ asset('assets/img/space.travel.land.jpg') }}" ><img height="60" src="{{ asset('assets/img/space.travel.land.jpg') }}"><a>
+			<a href="{{ asset('assets/img/robot.rocket.block.jpg') }}" ><img height="80" src="{{ asset('assets/img/robot.rocket.block.jpg') }}"><a>
+			<a href="{{ asset('assets/img/robot.star.space.jpg') }}" ><img height="80" src="{{ asset('assets/img/robot.star.space.jpg') }}"><a>
+			<a href="{{ asset('assets/img/space.travel.jpg') }}" ><img height="80" src="{{ asset('assets/img/space.travel.jpg') }}"><a>
+			<a href="{{ asset('assets/img/space.travel.land.jpg') }}" ><img height="80" src="{{ asset('assets/img/space.travel.land.jpg') }}"><a>
 		</div>
 	</div>
 	<div class="row">
 		<div class="col-md-10 col-md-offset-1">
-			<a href="{{ asset('assets/img/earth.kepler-20.jpg') }}" ><img height="60" src="{{ asset('assets/img/earth.kepler-20.jpg') }}"><a>
-			<a href="{{ asset('assets/img/earth.kepler-20.glaxy.jpg') }}" ><img height="60" src="{{ asset('assets/img/earth.kepler-20.glaxy.jpg') }}"><a>
-			<a href="{{ asset('assets/img/kepler.daison.jpg') }}" ><img height="60" src="{{ asset('assets/img/kepler.daison.jpg') }}"><a>
-			<a href="{{ asset('assets/img/kepler.daison.black.jpg') }}" ><img height="60" src="{{ asset('assets/img/kepler.daison.black.jpg') }}"><a>
+			<a href="{{ asset('assets/img/earth.kepler-20.jpg') }}" ><img height="80" src="{{ asset('assets/img/earth.kepler-20.jpg') }}"><a>
+			<a href="{{ asset('assets/img/earth.kepler-20.glaxy.jpg') }}" ><img height="80" src="{{ asset('assets/img/earth.kepler-20.glaxy.jpg') }}"><a>
+			<a href="{{ asset('assets/img/kepler.daison.jpg') }}" ><img height="80" src="{{ asset('assets/img/kepler.daison.jpg') }}"><a>
+			<a href="{{ asset('assets/img/kepler.daison.black.jpg') }}" ><img height="80" src="{{ asset('assets/img/kepler.daison.black.jpg') }}"><a>
 		</div>
 	</div>
 	<div class="row">
 		<div class="col-md-10 col-md-offset-1">
-			<a href="{{ asset('assets/img/human.robot.jpg') }}" ><img height="60" src="{{ asset('assets/img/human.robot.jpg') }}"><a>
+			<a href="{{ asset('assets/img/human.robot.jpg') }}" ><img height="80" src="{{ asset('assets/img/human.robot.jpg') }}"><a>
 		</div>
 	</div>
 </div>
