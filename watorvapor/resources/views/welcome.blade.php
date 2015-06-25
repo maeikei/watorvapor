f0005d2783b795d0a326a9cfe1decc7bec3d3dec
ma yingkui <maeikei@gmail.com>
2015-06-25 23:31:43 +0900

    change height

--- watorvapor/resources/views/welcome.blade.php
+++ watorvapor/resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
 	<div class="row" >
         <div class="col-md-10 col-md-offset-1">
-            大脑的研究
+            人脑基础研究
         </div>
 		<div class="col-md-10 col-md-offset-1">
 			<a href="{{ asset('assets/img/brain.orig.png') }}" ><img height="120"src="{{ asset('assets/img/brain.orig.png') }}"><a>
@@ -15,6 +15,9 @@
 		</div>
 	</div>
 	<div class="row" >
+        <div class="col-md-10 col-md-offset-1">
+            人工智能进化
+        </div>
 		<div class="col-md-10 col-md-offset-1">
 			<a href="{{ asset('assets/img/ai.nl.jpg') }}" ><img height="120"src="{{ asset('assets/img/ai.nl.jpg') }}"><a>
 			<a href="{{ asset('assets/img/ai.dnn.jpg') }}" ><img height="120"src="{{ asset('assets/img/ai.dnn.jpg') }}"><a>
@@ -23,6 +26,9 @@
 		</div>
 	</div>
 	<div class="row" >
+        <div class="col-md-10 col-md-offset-1">
+            大脑数据读取
+        </div>
 		<div class="col-md-10 col-md-offset-1">
 			<a href="{{ asset('assets/img/digital.Consciousness.reading.jpg') }}" ><img height="120"src="{{ asset('assets/img/digital.Consciousness.reading.jpg') }}"><a>
 			<a href="{{ asset('assets/img/digital.brain.block.jpg') }}" ><img height="120"src="{{ asset('assets/img/digital.brain.block.jpg') }}"><a>
@@ -32,6 +38,9 @@
 		</div>
 	</div>
 	<div class="row" >
+        <div class="col-md-10 col-md-offset-1">
+            人脑数据上传机器人
+        </div>
 		<div class="col-md-10 col-md-offset-1">
 			<a href="{{ asset('assets/img/robot.brain.vista.jpg') }}" ><img height="120"src="{{ asset('assets/img/robot.brain.vista.jpg') }}"><a>
 			<a href="{{ asset('assets/img/robot.brain.why.jpg') }}" ><img height="120"src="{{ asset('assets/img/robot.brain.why.jpg') }}"><a>
@@ -40,6 +49,9 @@
 		</div>
 	</div>
 	<div class="row" >
+        <div class="col-md-10 col-md-offset-1">
+            机器人宇航殖民
+        </div>
 		<div class="col-md-10 col-md-offset-1">
 			<a href="{{ asset('assets/img/robot.rocket.block.jpg') }}" ><img height="120"src="{{ asset('assets/img/robot.rocket.block.jpg') }}"><a>
 			<a href="{{ asset('assets/img/robot.star.space.jpg') }}" ><img height="120"src="{{ asset('assets/img/robot.star.space.jpg') }}"><a>
@@ -48,6 +60,9 @@
 		</div>
 	</div>
 	<div class="row" >
+        <div class="col-md-10 col-md-offset-1">
+            一百年后抵达开普勒星系，建立戴森球。
+        </div>
 		<div class="col-md-10 col-md-offset-1">
 			<a href="{{ asset('assets/img/earth.kepler-20.jpg') }}" ><img height="120"src="{{ asset('assets/img/earth.kepler-20.jpg') }}"><a>
 			<a href="{{ asset('assets/img/earth.kepler-20.glaxy.jpg') }}" ><img height="120"src="{{ asset('assets/img/earth.kepler-20.glaxy.jpg') }}"><a>
@@ -56,6 +71,9 @@
 		</div>
 	</div>
 	<div class="row" >
+        <div class="col-md-10 col-md-offset-1">
+            两百年后抵达宜居行星，重新组建生物人类。
+        </div>
 		<div class="col-md-10 col-md-offset-1">
 			<a href="{{ asset('assets/img/human.robot.jpg') }}" ><img height="120"src="{{ asset('assets/img/human.robot.jpg') }}"><a>
 		</div>
