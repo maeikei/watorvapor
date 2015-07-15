b08e463316d1aa4c192a0b625190dcc929979830
Ma Eikei <maeikei@gmail.com>
2015-07-16 00:05:52 +0900

    add ignore

--- watorvapor/resources/views/app.blade.php
+++ watorvapor/resources/views/app.blade.php
@@ -2,7 +2,7 @@
 <html>
     <head>
 	    <meta charset="utf-8">
-        <title>Watorvapor</title>
+        <title>Wator Vapor</title>
         <link href="//fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
         <link href="{{ asset('/css/app.css') }}" rel="stylesheet" type="text/css">
     </head>
