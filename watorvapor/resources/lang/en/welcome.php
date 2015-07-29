db0f9ce2f20abef13581d67ef30b011ff87f3e66
Ma Eikei <maeikei@gmail.com>
2015-07-29 23:07:05 +0900

    languange

--- watorvapor/resources/lang/en/welcome.php
+++ watorvapor/resources/lang/en/welcome.php
@@ -1,11 +1,11 @@
 <?php
 
 return [
-    'ThirtyYearBrainResearch' => '三十年后人脑基础研究完成',
-    'ThirtyYearStrongAI' => '三十年后进化出强人工智能',
-    'FortyYearBrainRead' => '四十年后大脑数据读取',
-    'FortyYearBrain2Robot' => '四十年后人脑数据上传机器人',
-    'FiftyYearRobot2Universe' => '五十年后机器人宇航殖民',
-    'HundredYearDason' => '一百年后抵达开普勒星系，建立戴森球',
-    'TwoHundredYearHuman' => '两百年后抵达宜居行星，重新组建生物人类',
+    'ThirtyYearBrainResearch' => 'Thirty years later complete the basic research of brain',
+    'ThirtyYearStrongAI' => 'Three decades later evolved artificial general intelligence',
+    'FortyYearBrainRead' => 'Forty years after read the data brain',
+    'FortyYearBrain2Robot' => 'Forty years later the robot brain data upload',
+    'FiftyYearRobot2Universe' => 'Fifty years after the robot colonial Aerospace',
+    'HundredYearDason' => 'One hundred years later we arrived at Kepler galaxy, build Dyson Sphere',
+    'TwoHundredYearHuman' => 'Two hundred years later arrival livable planet, the re-establishment of biological humanity',
 ];
---
+++ watorvapor/resources/lang/ja/welcome.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'ThirtyYearBrainResearch' => '30年後に、脳の基礎研究が完了',
+    'ThirtyYearStrongAI' => '三十年後に、強い人工知能まで進化',
+    'FortyYearBrainRead' => '四十年後に、脳のデータが読める',
+    'FortyYearBrain2Robot' => '40年後に、ロボットに脳データをアップロード',
+    'FiftyYearRobot2Universe' => '五十年後に、ロボットが宇宙植民へ',
+    'HundredYearDason' => '百年後に、ケプラー系に到着し、ダイソン球を構築',
+    'TwoHundredYearHuman' => '二百年後に、住める惑星へ、生物学的人類を読み返す',
+];
