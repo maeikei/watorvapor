7ca38046450b2b226d74d2c69a56fc75bc3476b3
Ma Eikei <maeikei@gmail.com>
2015-07-29 23:36:30 +0900

    languange

--- watorvapor/resources/views/welcome.blade.php
+++ watorvapor/resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 @extends('app')
 
 @section('content')
-<div class="container">
+<div class="container-content">
 	<div class="row" >
         <div class="col-md-10 col-md-offset-1">
             <div class="col-text">{{trans('welcome.ThirtyYearBrainResearch')}}</div>
